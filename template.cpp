d3112a66cb60c5cc506db13a66c3d298826514ff
Turbocyb0rg <turbocyborg7@gmail.com>
2024-02-07 12:23:33 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pil;
+typedef pair<long,long> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef unordered_map<ll,ll> umap;
+#define pb push_back 
+#define loop(i,a,b) for(ll i=a;i<b;i++) 
+#define For(i,n) for(int i=0;i<(ll)n;i++)
+#define Rev(i,n) for(int i=n-1;i>=0;i--)
+#define Rep(i,n) for(int =1;i<=n;++i)
+#define F first
+#define S second
+#define sort(v) sort(v.begin(),v.end())
+#define sort(a,n) sort(a,a+n)
+#define mp(a,b) make_pair(a,b)
+#define int ll
+
+void solve(){
+    ll n;  cin>>n;
+
+}
+
+signed main(){
+    ll t; cin>>t;  while(t--){
+        solve();
+       }
+    return 0;
+}
